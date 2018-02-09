d358c2447843c9441561a3dc28c310945cea05b2
wkcoder <997972547@qq.com>
2018-02-09 23:45:12 +0800

    bug

--- vue-music/src/common/js/jsonp.js
+++ vue-music/src/common/js/jsonp.js
@@ -1,6 +1,6 @@
 import originJSONP from 'jsonp'
 
-export default function jsonp(url, data, opt) {
+export default function jsonp(url, data, opt) { // bug
   url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
   return new Promise((resolve, reject) => {
     originJSONP(url, opt, (err, data) => {
